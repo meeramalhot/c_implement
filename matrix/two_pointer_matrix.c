1192b7bc3abac6140140438fc6335b60e40e7eb7
meera <meera.malhot@gmail.com>
2024-01-21 13:38:24 -0500

    add main function that asks for amnt of rows and columns

--- matrix/two_pointer_matrix.c
+++ matrix/two_pointer_matrix.c
@@ -2,3 +2,23 @@
 #include <stdlib.h>
 
 /* program that utilizes two pointers to create an 2D array */
+
+int matrix(rows, cols) {
+  
+
+}
+
+
+int main() {
+  int rows, cols = 0;
+  printf("enter amount of rows: ");
+  scanf("%d", &rows);
+
+  printf("\n");
+
+  printf("enter amount of columns: ");
+  scanf("%d", &cols);
+
+  printf("\n");
+
+}
