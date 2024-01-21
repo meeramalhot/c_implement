300df3e42ccf0d29871113d33d9e49fa05ebaf50
meera <meera.malhot@gmail.com>
2024-01-21 15:49:53 -0500

    .

--- matrix/two_pointer_matrix.c
+++ matrix/two_pointer_matrix.c
@@ -5,7 +5,7 @@
 
 int main() {
   //asking user for how many rows and cols and storing them
-  int rows, cols = 0;
+  int rows, cols;
   printf("enter amount of rows: ");
   scanf("%d", &rows);
 
@@ -15,21 +15,19 @@ int main() {
   scanf("%d", &cols);
 
   printf("\n");
-
- 
   
   int **matrix;
   //allocating memory for rows of the matrix
-  matrix = (int**)malloc((sizeof(int*) * rows));
+  matrix = (int **)malloc(rows * sizeof(int *));  
   for (int i = 0; i < rows; i++) {
       //allocating memory for columns of the matrix
       matrix[i] = (int *)malloc(cols * sizeof(int));
-    }
+  }
  
   //fill the matrix with random values 
   for (int i = 0; i<rows; i++){
     for (int j = 0; i<cols; j++){
-      int value = rand();
+      int value = i + j;
       matrix[i][j] = value;
     }
   }
