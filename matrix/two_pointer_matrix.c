cc929eca83c207766a587a760c997458be5f22fc
meera <meera.malhot@gmail.com>
2024-01-21 13:48:16 -0500

    used malloc to free space for matrix

--- matrix/two_pointer_matrix.c
+++ matrix/two_pointer_matrix.c
@@ -3,13 +3,16 @@
 
 /* program that utilizes two pointers to create an 2D array */
 
-int matrix(rows, cols) {
-  
+void matrix(rows, cols) {
+  int **matrix;
+  //allocating memory for the matrix
+  matrix = (int**)malloc(rows * (sizeof(int*)));
 
 }
 
 
 int main() {
+  //asking user for how many rows and cols and storing them
   int rows, cols = 0;
   printf("enter amount of rows: ");
   scanf("%d", &rows);
@@ -20,5 +23,7 @@ int main() {
   scanf("%d", &cols);
 
   printf("\n");
+
+  matrix(rows, cols);
 
 }
