d55962f6081dbcbf74beab5106ff5ca101606f8e
meera <meera.malhot@gmail.com>
2024-01-21 15:23:39 -0500

    need to check how memory is being allocated

--- matrix/two_pointer_matrix.c
+++ matrix/two_pointer_matrix.c
@@ -3,16 +3,29 @@
 
 /* program that utilizes two pointers to create an 2D array */
 
+int main(void) {
+  //asking user for how many rows and cols and storing them
+  int rows, cols = 0;
+  printf("enter amount of rows: ");
+  scanf("%d", &rows);
 
-int print_matrix(rows, cols) {
+  printf("\n");
+
+  printf("enter amount of columns: ");
+  scanf("%d", &cols);
+
+  printf("\n");
+
+ 
+  
   int **matrix;
   //allocating memory for rows of the matrix
   matrix = (int**)malloc((sizeof(int*) * rows));
-  //allocating memory for columns of the matrix
-  for (int i = 0; i<rows; i++){
-    matrix[i] = (int *)malloc(sizeof(int) * cols);
-  }
-
+  for (int i = 0; i < rows; i++) {
+      //allocating memory for columns of the matrix
+      matrix[i] = (int *)malloc(cols * sizeof(int));
+    }
+ 
   //fill the matrix with random values 
   for (int i = 0; i<rows; i++){
     for (int j = 0; i<cols; j++){
@@ -35,26 +48,6 @@ int print_matrix(rows, cols) {
   }
   //free memory used for rows
   free(matrix);
-
-  return(0);
-}
-
-
-
-int main(void) {
-  //asking user for how many rows and cols and storing them
-  int rows, cols = 0;
-  printf("enter amount of rows: ");
-  scanf("%d", &rows);
-
-  printf("\n");
-
-  printf("enter amount of columns: ");
-  scanf("%d", &cols);
-
-  printf("\n");
-
-  print_matrix(rows, cols);
 
   return(0);
 }
